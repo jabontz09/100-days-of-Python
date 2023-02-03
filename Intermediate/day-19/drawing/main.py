0226f2dc0fc6ca404303f115d87055d55f3a19a4
jabontz09 <jabontz09@gmail.com>
2023-02-03 08:27:46 -0500

    added day 19 projects

---
+++ Intermediate/day-19/drawing/main.py
@@ -0,0 +1,26 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+def move_forwards():
+    tim.forward(10)
+
+def move_backwards():
+    tim.backward(10)
+
+def turn_left():
+    new_heading = tim.heading() + 10
+    tim.setheading(new_heading)
+
+def turn_right():
+    new_heading = tim.heading() - 10
+    tim.setheading(new_heading)
+
+screen.listen()
+screen.onkey(key="w", fun=move_forwards)
+screen.onkey(move_backwards, "s")
+screen.onkey(turn_left, "a")
+screen.onkey(turn_right, "d")
+
+screen.exitonclick()
