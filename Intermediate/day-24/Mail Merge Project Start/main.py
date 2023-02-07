8c38781e768356ce5c0709134dc878b4119bd426
jabontz09 <jabontz09@gmail.com>
2023-02-07 11:01:24 -0500

    completed mail merge project

--- Intermediate/day-24/Mail Merge Project Start/main.py
+++ Intermediate/day-24/Mail Merge Project Start/main.py
@@ -2,7 +2,24 @@
 #for each name in invited_names.txt
 #Replace the [name] placeholder with the actual name.
 #Save the letters in the folder "ReadyToSend".
-    
+
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+def create_letters():
+    names = []
+    template = ""
+    with open("./Intermediate/day-24/Mail Merge Project Start/input/Names/invited_names.txt", "r") as f:
+        names = f.readlines()
+
+    with open("./Intermediate/day-24/Mail Merge Project Start/input/Letters/starting_letter.txt", "r") as f:
+        template = f.read()
+
+    for name in names:
+        name = name.strip()
+        with open(f"./intermediate/day-24/Mail Merge Project Start/Output/ReadyToSend/letter_for_{name}.txt", "w") as f:
+            content = template.replace("[name]", name)
+            f.write(content)
+
+create_letters()
